3d20e446deaf78eaa6cf455f834b69ee87f72c46
Nima Samadian <samadiannima@gmail.com>
2025-11-19 15:50:29 -0600

    Skeleton for MainApp.java

--- mainApp.java
+++ mainApp.java
@@ -1,3 +1,20 @@
-public class mainApp {
-    
+public class MainApp {
+
+    private DBManager dbManager;
+    private UIController uiController;
+    private DriveScanner driveScanner;
+
+    public MainApp() {              
+        dbManager = new DBManager(); //Initializes database manager and connect to DB
+        dbManager.connect(); 
+
+        driveScanner = new DriveScanner();  //Initializes drive scanner
+
+        uiController = new UIController(dbManager, driveScanner); //Initializes UI controller with DB manager and drive scanner
+    }
+
+    public static void main(String[] args) {
+        new MainApp(); 
+    }
 }
+
