fe129f0081453fcad79b44738b1f656b6d901bd1
Nima Samadian <samadiannima@gmail.com>
2025-11-19 20:00:11 -0600

    Added driveScanner skeleton

---
+++ driveScanner.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.util.List;
+
+public class DriveScanner {
+
+    private Drive rootDrive;
+
+    public DriveScanner() {         // scans files/folders and returns them
+        // consturctor
+    }
+
+    public List<FileItem> scan(Drive drive) {
+        return null;
+    }
+    private FileItem processFile(File file) {     // builds a FileItem object
+        return null;
+    }
+
+    public String getDriveSerial(String drivePath) {    // gets drive serial number
+        return null;
+    }
+
+    public boolean detectDrives() {
+        return false;
+    }
+}
