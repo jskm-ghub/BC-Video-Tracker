da92faed9a66651407682d6d616645371b2e9f67
Nathanael Leopold <ncleopold@gmail.com>
2025-12-01 22:58:47 -0600

    adding compiler, jar packaging, and jar running executables

--- compile-run_MAC.sh
+++ compile-run_MAC.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -e
 javac -cp src/lib/jsch-0.1.55.jar -d src/out src/java/*.java
 jar cfm VideoTrackerApplication.jar MANIFEST.MF -C src/out . -C src/images .
 java -jar ./VideoTrackerApplication.jar
